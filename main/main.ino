1861689e618322ec811321956a395c0d9796769e
Dolan Miu <dolan_miu@hotmail.com>
2017-11-18 23:21:10 +0000

    Initial commit

---
+++ main/main.ino
@@ -0,0 +1,12 @@
+int relayPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
